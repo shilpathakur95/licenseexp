718d50ca5c05bce3aa78e46d2d2346ee94723fd9
shilpathakur95 <thakur.shilpa95@gmail.com>
2018-06-14 21:28:54 +0530

    Add files via upload

--- login-page.js
+++ login-page.js
@@ -3,12 +3,12 @@
     var logInUrl = '%2Faccountiplogin%3FipId%3D2770%26accesskey%3Dj6i06kr8o8r';
     var primeBaseUrl = "https://captivateprime.adobe.com"
     var accId = 37949;
-    var isLocal = true; //set this flag when working locally
+    var isLocal = false; //set this flag when working locally
     var linkedinRedirectURI = '';
     if (isLocal == true)
         linkedinRedirectURI = 'http://localhost:8080/home.html';
     else
-        linkedinRedirectURI = 'https://s3.amazonaws.com/hosted_data/LinkedInPOC/linkedin-learner-exp2/home.html'; //hosted URL goes here
+        linkedinRedirectURI = 'https://learnerexp.herokuapp.com/home.html'; //hosted URL goes here
 
     var getURLParameter = function (sParam) {
         var sPageURL = decodeURIComponent(window.location.search.substring(1)),
@@ -31,13 +31,13 @@
         if (isLocal)
             linkedinRedirectURI = 'http://localhost:8080/my-learning.html';
         else
-            linkedinRedirectURI = 'https://s3.amazonaws.com/hosted_data/LinkedInPOC/linkedin-learner-exp2/my-learning.html'
+            linkedinRedirectURI = 'https://learnerexp.herokuapp.com/my-learning.html'
     } else if (redirectParameter == 'myCatalog') {
         if (isLocal)
             linkedinRedirectURI = 'http://localhost:8080/my-catalog.html';
         else
-            linkedinRedirectURI = 'https://s3.amazonaws.com/hosted_data/LinkedInPOC/linkedin-learner-exp2/my-catalog.html'
+            linkedinRedirectURI = 'https://learnerexp.herokuapp.com/my-catalog.html'
     }
 
-    window.location = primeBaseUrl + '/oauth/o/authorize?response_type=code&redirect_uri=' + linkedinRedirectURI + '&realm=1&client_id=' + app_id + '&scope=learner:read,learner:write&loginUrl=' + logInUrl;
+    window.location = primeBaseUrl + '/oauth/o/authorize?response_type=code&redirect_uri=' + linkedinRedirectURI + '&realm=1&client_id=' + app_id + '&scope=learner:read,learner:write';
 })();
